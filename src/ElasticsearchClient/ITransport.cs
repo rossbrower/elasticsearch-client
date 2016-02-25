1a658854e442615b753e2948f927b8bac8014f21
rossbrower <ross.alan.brower@gmail.com>
2016-02-25 12:55:46 -0800

    Delete ITransport.cs

--- src/ElasticsearchClient/ITransport.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Elasticsearch.Client
-{
-    // This project can output the Class library as a NuGet Package.
-    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public interface ITransport
-    {
-    }
-}
