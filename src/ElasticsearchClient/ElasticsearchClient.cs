3284471cb4bee6f7a7a67a4191653188916ead79
rossbrower <ross.alan.brower@gmail.com>
2016-03-10 12:01:03 -0800

    Fix synchronous overloads to avoid possible deadlocks.

--- src/ElasticsearchClient/ElasticsearchClient.cs
+++ src/ElasticsearchClient/ElasticsearchClient.cs
@@ -42,22 +42,38 @@ namespace Elasticsearch.Client
 
         private HttpResponseMessage Execute(string httpMethod, string uri)
         {
-            return ExecuteAsync(httpMethod, uri).Result;
+            return ExecuteAwait(httpMethod, uri).Result;
         }
 
         private HttpResponseMessage Execute(string httpMethod, string uri, Stream body)
         {
-            return ExecuteAsync(httpMethod, uri, body).Result;
+            return ExecuteAwait(httpMethod, uri, body == null
+                ? null
+                : new StreamContent(body)).Result;
         }
 
         private HttpResponseMessage Execute(string httpMethod, string uri, byte[] body)
         {
-            return ExecuteAsync(httpMethod, uri, body).Result;
+            return ExecuteAwait(httpMethod, uri, body == null
+                ? null
+                : new ByteArrayContent(body)).Result;
         }
 
         private HttpResponseMessage Execute(string httpMethod, string uri, string body)
         {
-            return ExecuteAsync(httpMethod, uri, body).Result;
+            return ExecuteAwait(httpMethod, uri, body == null
+                ? null
+                : new StringContent(body, Encoding.UTF8, "application/json")).Result;
+        }
+
+        private async Task<HttpResponseMessage> ExecuteAwait(string httpMethod, string uri, HttpContent content = null)
+        {
+            var client = mConnectionPool.GetClient();
+            using (var message = new HttpRequestMessage(new HttpMethod(httpMethod), uri))
+            {
+                message.Content = content;
+                return await client.SendAsync(message).ConfigureAwait(false);
+            }
         }
 
         private async Task<HttpResponseMessage> ExecuteAsyncImpl(string httpMethod, string uri, HttpContent content = null)
