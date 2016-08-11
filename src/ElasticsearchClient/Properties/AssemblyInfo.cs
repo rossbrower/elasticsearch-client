fd68c9232a76043ea8fb9df9ae75a02e28c675ef
ross.brower <ross.brower@RAB-XPS15>
2016-08-10 19:34:48 -0700

    manual merge of latest master.

--- src/ElasticsearchClient/Properties/AssemblyInfo.cs
+++ src/ElasticsearchClient/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -18,6 +17,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b195b916-e6e2-4004-9e3a-0903dfbda530")]
--- src/Tests/ResilienceTests.cs
+++ src/Tests/ResilienceTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,7 +19,8 @@ namespace Elasticsearch.Client.Tests
             using (var pool = new ConnectionPool(uris))
             {
                 var client = new ElasticsearchClient(pool);
-                await Task.WhenAll(Enumerable.Range(0, 100).Select(asi => client.ClusterHealthAsync()));
+                var responses = await Task.WhenAll(Enumerable.Range(0, 100).Select(asi => client.ClusterHealthAsync()));
+                PrintResults(responses);
             }
         }
 
@@ -25,15 +28,14 @@ namespace Elasticsearch.Client.Tests
         public async Task ConnectionPoolRandomFailures()
         {
             var uris = new[] {"http://localhost:9200", "http://localhost:9200" , "http://localhost:9200" };
-            var query = Encoding.UTF8.GetBytes("{\"match_all\":{}}");
-            using (var pool = new ConnectionPool(uris, new ExceptionDispatcher(), true))
+            using (var pool = new ConnectionPool(uris, new ExceptionDispatcher(new Dispatcher()), true))
             {
                 var client = new ElasticsearchClient(pool);
-                await Task.WhenAll(Enumerable.Range(0, 1000).Select(async i =>
+                var responses = await Task.WhenAll(Enumerable.Range(0, 1000).Select(async i =>
                 {
                     try
                     {
-                        return await client.SearchPostAsync(query);
+                        return await client.SearchPostAsync();
                     }
                     catch (RandomException)
                     {
@@ -42,36 +44,82 @@ namespace Elasticsearch.Client.Tests
                             ReasonPhrase = "Random!"
                         };
                     }
+                    catch (InvalidOperationException)
+                    {
+                        return new HttpResponseMessage(HttpStatusCode.NotFound)
+                        {
+                            ReasonPhrase = "No Servers"
+                        };
+                    }
                 }));
+                PrintResults(responses);
+            }
+        }
+
+        [Fact]
+        public async Task DispatcherTooManyRequestsRetry()
+        {
+            var client = new TooManyRequestsClient();
+            var dispatcher = new Dispatcher(10);
+            var responses = await Task.WhenAll(Enumerable.Range(0, 1000)
+                .Select(asi => dispatcher.ExecuteAsync(client, "GET", "http://nowhere")));
+            PrintResults(responses);
+        }
+
+        private static void PrintResults(HttpResponseMessage[] responses)
+        {
+            foreach (var group in responses.GroupBy(r => r.StatusCode))
+            {
+                Debug.WriteLine($"{group.Key}: {group.Count()}");   
             }
         }
 
         private class ExceptionDispatcher : IDispatcher
         {
             private readonly Random mRandom;
+            private readonly IDispatcher mBaseDispatcher;
 
-            public ExceptionDispatcher()
+            public ExceptionDispatcher(IDispatcher baseDispatcher)
             {
                 mRandom = new Random();
+                mBaseDispatcher = baseDispatcher;
             }
 
             public async Task<HttpResponseMessage> ExecuteAsync(HttpClient client, string httpMethod, string uri,
                 HttpContent content = null)
             {
-                var message = new HttpRequestMessage(new HttpMethod(httpMethod), uri)
+                var rand = mRandom.Next(20);
+                switch (rand)
                 {
-                    Content = content
-                };
-                if (mRandom.Next(20) == 1)
-                {
-                    throw new RandomException();
+                    case 1: throw new RandomException();
+                    case 2: return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
                 }
-                return await client.SendAsync(message).ConfigureAwait(false);
+                return await mBaseDispatcher.ExecuteAsync(client, httpMethod, uri, content).ConfigureAwait(false);
             }
         }
 
         private class RandomException : Exception
         {
+        }
+
+        private class TooManyRequestsClient: HttpClient
+        {
+            private readonly Random mRandom;
+
+            public TooManyRequestsClient()
+            {
+                mRandom = new Random();
+            }
+
+            public override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var rand = mRandom.Next(2);
+                if (rand == 1)
+                {
+                    return Task.FromResult(new HttpResponseMessage((HttpStatusCode) 429));
+                }
+                return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+            }
         }
     }
 }
